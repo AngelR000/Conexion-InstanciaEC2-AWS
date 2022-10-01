f3c9383cfeb19d9715235b54a9f9fd68f4a425df
AngelR000 <angel01680168@gmail.com>
2022-09-30 21:01:04 -0500

    clase

---
+++ bd.php
@@ -0,0 +1,25 @@
+<?php
+class Conexion {
+$servidor = "computaciondb.co361bfxv0nr.us-east-1.rds.amazonaws.com";
+$usuario = "admin";
+$contrasena = "AngeL-0108";
+$based = "ejemplo";
+$conn;
+
+public function conectar() {
+    $this->$conn = new mysqli(
+            $this->$servidor, $this->$usuario, $this->$contrasena, $this->$based
+    );
+    if ($this->$conn->connect_errno) {
+        echo "Fallo al contenctar a MySQL: (" . $this->$conn->connect_errno . ") " . $this->$conn->connect_error;
+    }
+    echo $this->$conn->host_info . "\n";
+}
+public function desconectar() {
+    self::conectar();
+    $this->$conn->close();
+    }
+}
+$ejemplo = new Conexion();
+$ejemplo->conectar();
+?>
--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<?php
-class Conexion {
-$servidor = "computaciondb.co361bfxv0nr.us-east-1.rds.amazonaws.com";
-$usuario = "admin";
-$contrasena = "AngeL-0108";
-$based = "ejemplo";
-$conn;
-
-public function conectar() {
-    $this->$conn = new mysqli(
-            $this->$servidor, $this->$usuario, $this->$contrasena, $this->$based
-    );
-    if ($this->$conn->connect_errno) {
-        echo "Fallo al contenctar a MySQL: (" . $this->$conn->connect_errno . ") " . $this->$conn->connect_error;
-    }
-    echo $this->$conn->host_info . "\n";
-}
-public function desconectar() {
-    self::conectar();
-    $this->$conn->close();
-    }
-}
-$ejemplo = new Conexion();
-$ejemplo->conectar();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,6 +6,9 @@ $ejemplo->conectar();
 
 <body>
     <center><h5>Hola, Servidor EC2</h5></center>
+    <?php
+    echo "PHP funciona";
+    ?>
 </body>
 
 </html>
