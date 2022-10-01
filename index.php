ccd8f2bc1aa8f10d576385c705c2bde9618599d3
AngelR000 <angel01680168@gmail.com>
2022-10-01 01:30:19 -0500

    Base y archivo php

--- index.php
+++ index.php
@@ -1,29 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-    
-<!--?php
-    $host='localhost';
+<?php
+    function Conectarse()
+    {
+    $host='db1angel.co361bfxv0nr.us-east-1.rds.amazonaws.com';
     $usuariodb='root';
     $passwdb='12341234';
-    $nombredb='db1';
-    if (!($link=mysql_connect($host,$usuariodb,$passwdb)))
+    $nombredb="db1angel";
+    if (!($link=mysqli_connect($host,$usuariodb,$passwdb)))
 {
 echo "Error conectando a la base de datos.";
 exit();
 }
-if (!mysql_select_db($nombredb,$link))
+if (!mysqli_select_db($link,$nombredb))
 {
 echo "Error seleccionando la base de datos, verifique que el nombre de usuario utilizado este asociado a la base de datos.";
 exit();
 }
 return $link;
-}
-
-$link=Conectarse();
-echo "Conexión con la base de datos conseguida.
-";
-mysql_close($link); //cierra la conexion
-    ?-->
+    }
+    $link=Conectarse();
+echo "Conexión con la base de datos conseguida.";
+?>
+<!DOCTYPE html>
+<html lang="sp">
 <head>
     <title>EC2 Server Angel</title>
 </head>
