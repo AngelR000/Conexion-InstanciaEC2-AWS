b86697b841208e3505a184ab0578c96da0e3fe0d
AngelR000 <angel01680168@gmail.com>
2022-09-30 20:35:56 -0500

    base de datos

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>EC2 Server Angel</title>
+</head>
+
+<body>
+<?php
+$servidor = "computaciondb.co361bfxv0nr.us-east-1.rds.amazonaws.com";
+$usuario = "admin";
+$contrasena = "AngeL-0108";
+$based = "ejemplo";
+$conn;
+
+public function conectar() {
+    $this->conn = new mysqli(
+            $this->servidor, $this->usuario, $this->contrasena, $this->based
+    );
+    if ($this->conn->connect_errno) {
+        echo "Fallo al contenctar a MySQL: (" . $this->conn->connect_errno . ") " . $this->conn->connect_error;
+    }
+    echo $this->conn->host_info . "\n";
+}
+public function desconectar() {
+    self::conectar();
+    $this->conn->close();
+}
+}
+$ejemplo = new Conexion();
+$ejemplo->conectar();
+
+?>
+
+    <center><h5>Hola, Servidor EC2</h5></center>
+</body>
+
+</html>
