862edfcfa5b40070642f9d7bfbfc9e3439599653
AngelR000 <angel01680168@gmail.com>
2022-10-01 12:58:41 -0500

    Base y archivo php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     function Conectarse()
     {
-    $host='db1angel.co361bfxv0nr.us-east-1.rds.amazonaws.com';
+    $host='db1angel.co361bfxv0nr.us-east-1.rds.amazonaws.com:3306';
     $usuariodb='root';
     $passwdb='12341234';
     $nombredb="db1angel";
