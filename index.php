385d00410b494168a50fbf766385969cb9e17bd3
AngelR000 <angel01680168@gmail.com>
2022-09-30 20:48:11 -0500

    clase

--- index.php
+++ index.php
@@ -15,17 +15,17 @@ $based = "ejemplo";
 $conn;
 
 public function conectar() {
-    $this->conn = new mysqli(
-            $this->servidor, $this->usuario, $this->contrasena, $this->based
+    $this->$conn = new mysqli(
+            $this->$servidor, $this->$usuario, $this->$contrasena, $this->$based
     );
-    if ($this->conn->connect_errno) {
-        echo "Fallo al contenctar a MySQL: (" . $this->conn->connect_errno . ") " . $this->conn->connect_error;
+    if ($this->$conn->connect_errno) {
+        echo "Fallo al contenctar a MySQL: (" . $this->$conn->connect_errno . ") " . $this->$conn->connect_error;
     }
-    echo $this->conn->host_info . "\n";
+    echo $this->$conn->host_info . "\n";
 }
 public function desconectar() {
     self::conectar();
-    $this->conn->close();
+    $this->$conn->close();
     }
 }
 $ejemplo = new Conexion();
